7d4ec7563383aa769c5170a95572532bf16a8e4d
carlos <42757882+carordo@users.noreply.github.com>
2020-04-25 14:18:21 -0400

    modified circles

---
+++ graph1.js
@@ -0,0 +1,272 @@
+
+// get the data
+links =
+[
+  {"source":"Liverpool FC","target":"Norwich City FC","value":1,"points":3},{"source":"Burnley FC","target":"Southampton FC","value":1,"points":3},{"source":"Tottenham Hotspur FC","target":"Aston Villa FC","value":1,"points":3},{"source":"Manchester United FC","target":"Chelsea FC","value":1,"points":3},{"source":"Sheffield United FC","target":"Crystal Palace FC","value":1,"points":3},{"source":"Liverpool FC","target":"Arsenal FC","value":1,"points":3},{"source":"Leicester City FC","target":"AFC Bournemouth","value":1,"points":3},{"source":"Manchester City FC","target":"Brighton & Hove Albion FC","value":1,"points":3},{"source":"Liverpool FC","target":"Newcastle United FC","value":1,"points":3},{"source":"Manchester United FC","target":"Leicester City FC","value":1,"points":3},{"source":"Tottenham Hotspur FC","target":"Crystal Palace FC","value":1,"points":3},{"source":"Leicester City FC","target":"Tottenham Hotspur FC","value":1,"points":3},{"source":"Manchester City FC","target":"Watford FC","value":1,"points":3},{"source":"Burnley FC","target":"Norwich City FC","value":1,"points":3},{"source":"Chelsea FC","target":"Brighton & Hove Albion FC","value":1,"points":3},{"source":"Tottenham Hotspur FC","target":"Southampton FC","value":1,"points":3},{"source":"Leicester City FC","target":"Newcastle United FC","value":1,"points":3},{"source":"Liverpool FC","target":"Leicester City FC","value":1,"points":3},{"source":"Burnley FC","target":"Everton FC","value":1,"points":3},{"source":"Chelsea FC","target":"Newcastle United FC","value":1,"points":3},{"source":"Leicester City FC","target":"Burnley FC","value":1,"points":3},{"source":"Sheffield United FC","target":"Arsenal FC","value":1,"points":3},{"source":"Manchester City FC","target":"Aston Villa FC","value":1,"points":3},{"source":"Liverpool FC","target":"Tottenham Hotspur FC","value":1,"points":3},{"source":"Manchester City FC","target":"Southampton FC","value":1,"points":3},{"source":"Sheffield United FC","target":"Burnley FC","value":1,"points":3},{"source":"Chelsea FC","target":"Crystal Palace FC","value":1,"points":3},{"source":"Leicester City FC","target":"Arsenal FC","value":1,"points":3},{"source":"Burnley FC","target":"West Ham United FC","value":1,"points":3},{"source":"Liverpool FC","target":"Manchester City FC","value":1,"points":3},{"source":"Manchester United FC","target":"Brighton & Hove Albion FC","value":1,"points":3},{"source":"Manchester City FC","target":"Chelsea FC","value":1,"points":3},{"source":"Liverpool FC","target":"Brighton & Hove Albion FC","value":1,"points":3},{"source":"Tottenham Hotspur FC","target":"AFC Bournemouth","value":1,"points":3},{"source":"Leicester City FC","target":"Everton FC","value":1,"points":3},{"source":"Leicester City FC","target":"Watford FC","value":1,"points":3},{"source":"Manchester United FC","target":"Tottenham Hotspur FC","value":1,"points":3},{"source":"Chelsea FC","target":"Aston Villa FC","value":1,"points":3},{"source":"Liverpool FC","target":"Everton FC","value":1,"points":3},{"source":"Tottenham Hotspur FC","target":"Burnley FC","value":1,"points":3},{"source":"Liverpool FC","target":"Watford FC","value":1,"points":3},{"source":"Sheffield United FC","target":"Aston Villa FC","value":1,"points":3},{"source":"Burnley FC","target":"Newcastle United FC","value":1,"points":3},{"source":"Manchester City FC","target":"Leicester City FC","value":1,"points":3},{"source":"Manchester United FC","target":"Newcastle United FC","value":1,"points":3},{"source":"Tottenham Hotspur FC","target":"Brighton & Hove Albion FC","value":1,"points":3},{"source":"Liverpool FC","target":"Wolverhampton Wanderers FC","value":1,"points":3},{"source":"Manchester City FC","target":"Sheffield United FC","value":1,"points":3},{"source":"Manchester City FC","target":"Everton FC","value":1,"points":3},{"source":"Liverpool FC","target":"Sheffield United FC","value":1,"points":3},{"source":"Sheffield United FC","target":"West Ham United FC","value":1,"points":3},{"source":"Chelsea FC","target":"Burnley FC","value":1,"points":3},{"source":"Manchester United FC","target":"Norwich City FC","value":1,"points":3},{"source":"Burnley FC","target":"Leicester City FC","value":1,"points":3},{"source":"Liverpool FC","target":"Manchester United FC","value":1,"points":3},{"source":"Leicester City FC","target":"West Ham United FC","value":1,"points":3},{"source":"Tottenham Hotspur FC","target":"Norwich City FC","value":1,"points":3},{"source":"Liverpool FC","target":"Southampton FC","value":1,"points":3},{"source":"Tottenham Hotspur FC","target":"Manchester City FC","value":1,"points":3},{"source":"Manchester City FC","target":"West Ham United FC","value":1,"points":3},{"source":"Sheffield United FC","target":"AFC Bournemouth","value":1,"points":3},{"source":"Chelsea FC","target":"Tottenham Hotspur FC","value":1,"points":3},{"source":"Liverpool FC","target":"West Ham United FC","value":1,"points":3},{"source":"Manchester United FC","target":"Watford FC","value":1,"points":3},{"source":"Burnley FC","target":"AFC Bournemouth","value":1,"points":3},{"source":"Chelsea FC","target":"Everton FC","value":1,"points":3},{"source":"Leicester City FC","target":"Aston Villa FC","value":1,"points":3},{"source":"Liverpool FC","target":"AFC Bournemouth","value":1,"points":3},{"source":"Manchester United FC","target":"Manchester City FC","value":1,"points":3},{"source":"Sheffield United FC","target":"Norwich City FC","value":1,"points":3},{"source":"Manchester City FC","target":"West Ham United FC","value":-1,"points":3},{"source":"Liverpool FC","target":"Southampton FC","value":-1,"points":3},{"source":"Leicester City FC","target":"Sheffield United FC","value":-1,"points":3},{"source":"Chelsea FC","target":"Norwich City FC","value":-1,"points":3},{"source":"Manchester City FC","target":"AFC Bournemouth","value":-1,"points":3},{"source":"Liverpool FC","target":"Burnley FC","value":-1,"points":3},{"source":"Chelsea FC","target":"Wolverhampton Wanderers FC","value":-1,"points":3},{"source":"Sheffield United FC","target":"Everton FC","value":-1,"points":3},{"source":"Liverpool FC","target":"Chelsea FC","value":-1,"points":3},{"source":"Manchester City FC","target":"Everton FC","value":-1,"points":3},{"source":"Liverpool FC","target":"Sheffield United FC","value":-1,"points":3},{"source":"Chelsea FC","target":"Southampton FC","value":-1,"points":3},{"source":"Manchester City FC","target":"Crystal Palace FC","value":-1,"points":3},{"source":"Leicester City FC","target":"Southampton FC","value":-1,"points":3},{"source":"Chelsea FC","target":"Burnley FC","value":-1,"points":3},{"source":"Manchester United FC","target":"Norwich City FC","value":-1,"points":3},{"source":"Liverpool FC","target":"Aston Villa FC","value":-1,"points":3},{"source":"Chelsea FC","target":"Watford FC","value":-1,"points":3},{"source":"Leicester City FC","target":"Crystal Palace FC","value":-1,"points":3},{"source":"Liverpool FC","target":"Crystal Palace FC","value":-1,"points":3},{"source":"Tottenham Hotspur FC","target":"West Ham United FC","value":-1,"points":3},{"source":"Burnley FC","target":"Watford FC","value":-1,"points":3},{"source":"Leicester City FC","target":"Brighton & Hove Albion FC","value":-1,"points":3},{"source":"Manchester City FC","target":"Burnley FC","value":-1,"points":3},{"source":"Liverpool FC","target":"AFC Bournemouth","value":-1,"points":3},{"source":"Manchester United FC","target":"Manchester City FC","value":-1,"points":3},{"source":"Sheffield United FC","target":"Norwich City FC","value":-1,"points":3},{"source":"Leicester City FC","target":"Aston Villa FC","value":-1,"points":3},{"source":"Manchester City FC","target":"Arsenal FC","value":-1,"points":3},{"source":"Tottenham Hotspur FC","target":"Wolverhampton Wanderers FC","value":-1,"points":3},{"source":"Liverpool FC","target":"West Ham United FC","value":-1,"points":3},{"source":"Burnley FC","target":"AFC Bournemouth","value":-1,"points":3},{"source":"Sheffield United FC","target":"Brighton & Hove Albion FC","value":-1,"points":3},{"source":"Chelsea FC","target":"Tottenham Hotspur FC","value":-1,"points":3},{"source":"Liverpool FC","target":"Leicester City FC","value":-1,"points":3},{"source":"Leicester City FC","target":"West Ham United FC","value":-1,"points":3},{"source":"Manchester United FC","target":"Burnley FC","value":-1,"points":3},{"source":"Chelsea FC","target":"Arsenal FC","value":-1,"points":3},{"source":"Leicester City FC","target":"Newcastle United FC","value":-1,"points":3},{"source":"Liverpool FC","target":"Tottenham Hotspur FC","value":-1,"points":3},{"source":"Manchester City FC","target":"Aston Villa FC","value":-1,"points":3},{"source":"Manchester City FC","target":"Sheffield United FC","value":-1,"points":3},{"source":"Burnley FC","target":"Manchester United FC","value":-1,"points":3},{"source":"Liverpool FC","target":"Wolverhampton Wanderers FC","value":-1,"points":3},{"source":"Sheffield United FC","target":"Crystal Palace FC","value":-1,"points":3},{"source":"Tottenham Hotspur FC","target":"Aston Villa FC","value":-1,"points":3},{"source":"Manchester United FC","target":"Chelsea FC","value":-1,"points":3},{"source":"Burnley FC","target":"Southampton FC","value":-1,"points":3},{"source":"Liverpool FC","target":"Norwich City FC","value":-1,"points":3},{"source":"Manchester City FC","target":"Leicester City FC","value":-1,"points":3}
+];
+
+var nodes = {}; // Empty information to initialize
+
+// compute the distinct nodes from the links.
+// carlos note: update them or add them
+links.forEach(function(link) {
+    link.source = nodes[link.source] ||
+        (nodes[link.source] = {name: link.source}); // if we get that one repeated we don't get extra nodes
+    link.target = nodes[link.target] ||
+        (nodes[link.target] = {name: link.target}); //if you were not in the source but were in the target we will add
+});
+
+var width = 1100,
+    height = 800
+    heightLetters = 20;
+
+
+
+
+// this constructor that create the nodes and links
+
+var force = d3.forceSimulation()
+
+    .nodes(d3.values(nodes))
+    .force("link", d3.forceLink(links).distance(500))
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force("x", d3.forceX())
+    .force("y", d3.forceY())
+    .force("charge", d3.forceManyBody().strength(-50))
+    .alphaTarget(1)
+    .on("tick", tick);
+
+// This function contains the data in links
+// I see no difference between d and i
+//console.log (links, function(d,i) {return i })
+//console.log (links, function(d,i) {return d })
+
+// carlos note use the following as canvas on html
+var svg = d3.select("body").append("svg")
+
+    .attr("width", width)
+    .attr("width", width)
+    .attr("height", height)
+    // .attr("transform", "translate(0, 150)")
+    // .attr("viewBox", "0 0 900 900");
+      //
+      // <rect width='500' height='500' transform='translate(295 115)' />
+      ;
+// carlos note:
+
+// add the links and the arrows
+var path = svg.append("g")
+    .selectAll("path")
+    .data(links)
+    .enter()
+    .append("path")
+    .attr("class", function(d) { return "link " + d.type; })
+
+    .style ("stroke", function (d){
+    return d.value == 1 ? "gray":      "gray";
+    })
+    .style ("stroke-width", function (d){
+    return d.value == 1 ? 2 :      1;
+    })
+    .style ("stroke-dasharray", function (d){
+    return d.value == -1 ? 5: "none";
+    })
+    //
+    // .style ("stroke", function (d){
+    //   // if (d.value == -1) {
+      //
+      //     return "#8D5524"
+      // // } else if (d.value == 0){
+      // //     return "#8E4E4C"
+      //
+      // }else if (d.value == 1){
+      //
+      //   return "#FFDBAC"
+      // }})
+    //
+    // return d.value == -1 ? "red" :      "none";
+    // })
+    // .style ("stroke-width", function (d){
+    // return d.value == 1 ? 2:      1;
+    // })
+    // .style ("stroke", function (d){
+    //
+    // return d.value == -1 ? "red" :      "none";
+    // })
+    // .style ("stroke-dasharray", function (d){
+    // return d.value == 0 ? 5: "none";
+    // })
+    // .style ("stroke-linecap", function (d){
+    // return d.value == -2 ? 5: "none";
+    // })
+    // .style ("stroke", function (d){
+    // return d.value == -2 ? "red": "none";
+    // })
+    ;
+
+
+
+
+// define the nodes
+var node = svg.selectAll(".node")
+    .data(force.nodes())
+    .enter().append("g");
+
+var link = svg.selectAll('path.link')
+    .data(links)
+    .enter()
+    .append("g")
+
+
+var nodesCircle = node.append("circle")
+   .attr("class", "node")
+   .attr("r", function(d) {
+   // d.weight = links.filter(function(l) {
+   // return l.source.index == d.index || l.target.index == d.index
+   // }).length;
+
+   d.weight = links.filter(function(l) {
+   return l.source.index == d.index
+   // || l.target.index == d.index
+   }).length;
+   //
+   // d.weight = links.filter(function(l) {
+     // l.source.index == d.index
+
+  //
+ //   if (l.value > 0) {
+ //    console.log(l.source)
+ //    console.log(l.value)
+ //    console.log(d.index)
+ //    console.log(l.value == d.index)
+ //     return l.value == d.index
+ //
+ //   }
+ //   else if (l.value ==-1){
+ //    // console.log("lvalue2")
+ //     return Math.sqrt(l.value)
+ //
+ //   }
+ //  else if (l.value ==0){
+ //     return (l.value )
+ //   }
+ //    //
+ //    // return l.value
+ //
+ //    // console.log(l.value)
+ // // });
+ // }).length;
+   // return l.scaleSqrt().range([])
+  // console.log(d.index)
+  // console.log(d.weight)
+   var minRadius =2;
+   return minRadius * d.weight^2;
+   })
+   .on("dblclick",dblclick)
+   .call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+
+var arrayNodes = d3.values(nodes);
+let resultNodes = arrayNodes.map(function(d) { return d.weight});
+
+var nodeMin = d3.min(resultNodes);
+var nodeMax = d3.max(resultNodes);
+
+var colorScale = d3.scaleSequential()
+        .domain([nodeMin, nodeMax])
+        .interpolator(d3.interpolateReds);
+
+nodesCircle.style( "fill", function(d) {
+        return colorScale(d.weight);
+   });
+
+
+
+node.append("text")
+    .attr ("dx", function(d) {
+            return d.weight**2*.1+5})
+    .attr ("dy", ".90em")
+    .text(function (d) { return d.name });
+
+
+// add the curvy lines
+function tick() {
+/*
+    text.attr("x", d -> d.x )
+    text.attr("y", d -> d.y )
+*/
+    path.attr("d", function(d) {
+        var dx = d.target.x - d.source.x,
+            dy = d.target.y - d.source.y,
+            dr = Math.sqrt(dx * dx + dy * dy);
+        return "M" +
+            d.source.x + "," +
+            d.source.y + "A" +
+            dr + "," + dr + " 0 0,1 " +
+            d.target.x + "," +
+            d.target.y;
+    });
+
+    node
+        .attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")"; })
+};
+
+// Pin the nodes
+function dblclick(d){
+console.log("Before")
+console.log(d.fixed)
+
+    if (d.fixed == true) {
+         d3.select(this).classed ("fixed",d.fixed = false).style("fill",  function(d) {
+            return colorScale(d.weight);
+         });
+         d.fx = null;
+         d.fy = null;
+         }
+
+    else {d3.select(this).classed ("fixed",d.fixed = true).style("fill", "orange");
+
+        d.fx = d.x;
+        d.fy = d.y;
+        }
+
+    console.log("After")
+    console.log(d.fixed)
+
+};
+
+// Creates the name cordonez top right
+svg.append("g")
+    .attr("transform", "translate(0," + heightLetters + ")")
+    .append("text")
+    .attr("fill", "#000")
+    .attr("x", 150)
+    .attr("dy", "3em")
+    .attr("text-anchor", "end")
+    .text("Graph");
+
+function dragstarted(d) {
+if (!d3.event.active) force.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+    };
+
+function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+    };
+
+function dragended(d) {
+    if (!d3.event.active) force.alphaTarget(0);
+    if (d.fixed == true) {
+        d.fx = d.x;
+        d.fy = d.y;
+}
+    else {
+        d.fx = null;
+        d.fy = null;
+}
+};
